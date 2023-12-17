d932c267cbdb3c371341df533980522252abb373
cshd <clement.sainthilaire@sts-sio-caen.info>
2023-12-17 20:48:28 +0100

    Mise en place du projet, création de la base de donnée.

---
+++ php/accueil.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="../css/composants.css" href="style.css">  
+    <title>csh</title>
+</head>
+<body>
+
+    <?php include 'php/composants/header.html'; ?>
+
+</body>
+</html>
