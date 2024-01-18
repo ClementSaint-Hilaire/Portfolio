8ec722f7fbfc63fc3a7635748bc060139559bdfe
cshd <clement.sainthilaire@sts-sio-caen.info>
2024-01-18 17:56:38 +0100

    — Corectif— ajtustement du responsive du header

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles.css">  
-    <title>csh</title>
-</head>
-<body>
-    <?php
-        require_once './configuration/database.php';
-    ?>
-    <?php
-        include './php/accueil.php'
-    ?>
-</body>
-</html>
+<h1>tu fais quoi là ?</h1>
