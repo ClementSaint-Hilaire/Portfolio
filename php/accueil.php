4b54e6fe5dd173a7605316fdd6a36fdf2af59780
cshd <clement.sainthilaire@sts-sio-caen.info>
2024-01-18 20:47:43 +0100

    — Corectif— footer

--- php/accueil.php
+++ php/accueil.php
@@ -3,11 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/accueil.css">  
     <title>csh</title>
 </head>
 <body>
-
+    
     <?php include 'php/composants/header.html'; ?>
+
+
+    <div class="test"></div>
+
+
+    <?php include 'php/composants/footer.html'; ?>
 
 </body>
 </html>
