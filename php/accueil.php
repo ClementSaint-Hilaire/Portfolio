25e07cdefc75e0df082bb453e9e194ea353e5086
cshd <clement.sainthilaire@sts-sio-caen.info>
2024-01-20 15:47:41 +0100

    — Ajout — Font chillax

--- php/accueil.php
+++ php/accueil.php
@@ -3,18 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="..css/styles.css">
     <link rel="stylesheet" href="../css/accueil.css">  
     <title>csh</title>
 </head>
 <body>
     
-    <?php include 'php/composants/header.html'; ?>
+    <?php include 'composants/header.html'; ?>
 
 
     <div class="test"></div>
 
 
-    <?php include 'php/composants/footer.html'; ?>
+    <?php include 'composants/footer.html'; ?>
 
 </body>
 </html>
