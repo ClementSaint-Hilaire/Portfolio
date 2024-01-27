beeea53cc9db428e3d8d610bca1acf1cf5ec7969
cshd <clement.sainthilaire@sts-sio-caen.info>
2024-01-27 18:52:57 +0100

    — Ajout — projet "quantum computer"

---
+++ php/projets.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/projets.css">  
+    <title>csh</title>
+</head>
+<body>
+    
+    <?php include 'composants/header.html'; ?>
+    
+    <section class="titreEtSousTitre">
+        <div class="containerTitreEtSousTitre">
+            <h1><span>Mes</span>projets</h1>
+            <h2>PROJETS D’ENTREPRISE, PROJETS PERSONNELS, TRAVAUX ET CRÉATIONS.</h2>
+        </div>
+    </section>
+
+    <section class="projet">
+
+        <div class="quantumContainer">
+            <div class="containerImageEtTexte">
+                <img src="../images/Projets/quantumComupter.png" alt="Quantum Computer">
+                <div class="containerText">
+                    <div class="elementsTextContainer">
+                        <h1>reSearch on </h1>
+                        <h2>Quantum Computer</h2>
+                        <h3>made by csh</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="footerLientoProjet">
+                 <h1>Ouvrir le détail sur Figma (soon)</h1>
+                 <a href="#"><img src="../images/Projets/bouton.svg" alt=""></a>
+            </div>
+        </div>
+
+    </section>
+
+
+    <?php include 'composants/footer.html'; ?>
+
+</body>
+</html>
