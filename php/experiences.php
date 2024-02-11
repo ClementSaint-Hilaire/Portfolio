a6805b9b220e7892bc79af1e251710d70763af47
cshd <clement.sainthilaire@sts-sio-caen.info>
2024-02-11 18:07:35 +0100

    --ajout-- favicon

--- php/experiences.php
+++ php/experiences.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="icon" type="image/png" href="../images/logo.png" />
     <link rel="stylesheet" href="../css/experiences.css">  
     <title>csh</title>
 </head>
